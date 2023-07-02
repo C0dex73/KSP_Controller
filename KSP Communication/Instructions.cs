81db212439e3062e675bce9733c036f9d5b37a6d
C0dex73 <c0d3x73@gmail.com>
2023-07-02 10:30:00 +0200

    vs setup modified
    now have pull extension

--- KSP Communication/Instructions.cs
+++ KSP Communication/Instructions.cs
@@ -5,6 +5,7 @@
  * **************************** IMPORTANT ****************************
  * Krcp needed
  * Read commands.txt to know how to communicate with it
+ * Use config.cfg or type config in the console at start to setup your environnement
  * 
  * THIS IS NOT THE MAIN PROGRAM !
  * VOID MAIN() IS IN PROGRAM.CS
