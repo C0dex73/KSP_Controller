2a142da8189acef251e76b41d6281089ca6f9793
Codex73 <95096929+C0dex73@users.noreply.github.com>
2023-07-22 21:14:58 +0200

    removed temp commentaries in resistorCheck.ino

--- ResistorCheck/ResistorCheck.ino
+++ ResistorCheck/ResistorCheck.ino
@@ -4,13 +4,13 @@
  * FOUND IT : 33K / 100K / 150K
  */
 
-#define RES33K [880,900]		//
-#define RES100K [690, 710]		//
-#define RES150K [600, 620]      //
+#define RES33K [880,900]
+#define RES100K [690, 710]
+#define RES150K [600, 620]
 #define RESFULL [930, 940]
 #define RES33K100K [915, 925]
-#define RES100K150K [795, 815]	//
-#define RES150K33K [905, 914]	//
+#define RES100K150K [795, 815]
+#define RES150K33K [905, 914]
 
 const int READRES_Pin = A0;
 
